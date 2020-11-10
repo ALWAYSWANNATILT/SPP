b12845a1758a6f6c4a6633a8fc23bcc422a15e82
Artyom <47700409+ALWAYSWANNATILT@users.noreply.github.com>
2020-11-11 00:01:58 +0300

    Add files via upload

---
+++ lab3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+   
+        {
+            Mnoj first = new Mnoj();
+            Mnoj second = new Mnoj();
+
+            Console.WriteLine(first.Equals(second));
+            first.Equals(second);
+            first.Add(2);
+            first.Connect(second);
+            first.Search(1);
+            first.Show();
+
+            Console.ReadKey();
+        }
+    }
+    public class Mnoj
+    { 
+        public int[] mnojestvo;
+ 
+        public Mnoj()
+        { 
+            mnojestvo = new int[0];
+            Console.WriteLine("Enter number/ q to stop: ");
+            string temp = "";
+            int Temp, size = 0;
+            while (true)
+            {
+                temp = Console.ReadLine();
+                bool check = int.TryParse(temp, out Temp);
+                if (check)
+                {
+               
+                    Array.Resize(ref mnojestvo, ++size);
+                    mnojestvo[size-1] = Temp;
+                }
+                if (temp == "q") break;
+
+            }
+        }
+
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+            Mnoj temp = obj as Mnoj;//проверка на принадлежность классу Equila
+            if (mnojestvo.Length != temp.mnojestvo.Length)
+                return false;
+            for (int i = 0; i < mnojestvo.Length; i++)
+            {
+                if (mnojestvo[i] != temp.mnojestvo[i])
+                    return false;
+            }
+            return true;
+        }
+
+
+        public void Show()
+        {
+            for (int i = 0; i < mnojestvo.Length; i++)
+            {
+                Console.WriteLine($"{mnojestvo[i]}");
+            }
+        }
+
+        public void Add(int num)
+        {
+            for (int i = 0; i < mnojestvo.Length; i++)
+            {
+                if(mnojestvo[i] == num)
+                {
+                    Console.WriteLine("Данное число есть в множестве");
+                    return;
+                }
+                
+            }
+            Array.Resize(ref mnojestvo, mnojestvo.Length + 1);
+            mnojestvo[mnojestvo.Length - 1] = num;
+
+        }
+
+        public void Delete(int number)
+        {
+            List<int> list = mnojestvo.ToList<int>();
+            list.RemoveAt(number - 1);
+            mnojestvo = list.ToArray();
+        }
+
+        public void Connect(Mnoj mnoj)
+        {
+            bool check;    
+            int[] arr = mnojestvo;
+            for (int i = 0; i < mnoj.mnojestvo.Length; i++)
+            {   
+                check = true;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (mnoj.mnojestvo[i] == arr[j])
+                        check = false;
+                }
+                if (check) 
+                { 
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = mnoj.mnojestvo[i];
+                }
+
+            }
+            Array.Sort(arr);
+            mnojestvo = arr;
+        } 
+
+        public void Search(int number)
+        {
+            bool check = false;
+            for (int i = 0; i < mnojestvo.Length; i++)
+            {
+               if(mnojestvo[i] == number)    
+                {
+                    check = true; break;
+                }
+               
+            }
+            if (check) Console.WriteLine("сИМВОЛ ЕСТЬ!!!!");
+            else Console.WriteLine("СИМВОЛА НЕТ((");
+        }
+
+    }
+
+}
---
+++ lab3/secondPart/secondPart/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace secondPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Disk disk = new Disk();
+            disk.Add(new File(100000, "asd", "txt"));
+            disk.Add(new File(100000, "zxc", "txt"));
+            disk.Add(new File(500000, "xcv", "txt"));
+            disk.Add(new File(200000, "cvb", "txt"));
+            disk.Show();
+            disk.Delete("asd");
+            disk.Delete("xcv");
+            disk.Add(new File(400000, "xvcb", "txt"));
+            disk.Show();
+        }
+    }
+    public class Disk
+    {
+        List<File> Files = new List<File>();
+        const int Size = 1440000;
+
+        public Disk() { } 
+        public void Delete(string _name)
+        {
+            for(int i = 0; i < Files.Count; i++)
+            {
+                if (Files[i].name == _name) Files.RemoveAt(i);
+            }
+        }
+        public void Show()
+        {
+            foreach(File file in Files)
+            {
+                Console.WriteLine($" Имя Файла {file.name}| РАзмер {file.size}| Расширениe {file.exp}| НАчало и Конец {file.Psize} {file.Nsize}");
+            }
+        }
+        public void Add(File file)
+        {
+            int size = file.size;
+            int temp = 0;
+            if(Files.Count == 0)
+            {
+                file.Psize = 0;
+                file.Nsize = file.Psize + file.size;
+                Files.Add(file);
+                Console.WriteLine("Файл успешно создан");
+                return;
+            }
+            temp = Files[0].Psize;
+            for (int i = 0; i < Files.Count; i++)
+            {
+                if (i == Files.Count - 1)
+                {
+                    if (Files[i].Psize - temp > size) 
+                    {
+                       
+                        file.Psize = temp;
+                        file.Nsize = temp + file.size;
+                
+                        Files.Insert(i, file);
+
+                        Console.WriteLine("Файл успешно создан");
+                        return;
+                    } 
+                    int TempSize = Size - Files[i].Nsize;
+                    if (file.size < TempSize)
+                   {
+                        file.Psize = Files[i].Nsize + 1;
+                        file.Nsize = file.Psize + file.size;
+                        Files.Add(file);
+                        Console.WriteLine("Файл успешно создан");
+                                           
+                    }
+                    else Console.WriteLine("Нет места дял файла");
+                    return;
+                }
+                if (Files[i].Psize == temp)
+                {
+                    temp = Files[i].Nsize + 1;
+                    continue;
+                }
+                else
+                {
+                    if (Files[i].Psize - temp > size)
+                    {
+                        //file.Psize = Files[i].Nsize;
+                        file.Psize = temp;
+                        file.Nsize = temp + file.size;
+                        // file.Nsize = file.Psize + file.size;
+                        Files.Insert(i , file);
+
+                        Console.WriteLine("Файл успешно создан");
+                        return;
+                    }
+                    
+                    else
+                    {
+                        temp = Files[i].Nsize + 1;
+                    }
+                }             
+            }
+      
+        }
+        public void SizeCheck()
+        {
+            int curSize = 0;
+            foreach (File file in Files)
+            {
+                curSize += file.GetSize();
+                if (curSize > Size) Console.WriteLine("Нет места");
+            }
+        }
+    }
+    public class File
+    {
+        public string name;
+        public string exp;
+        public int size;
+        public int Psize;
+        public int Nsize;
+        public File(int size, string name, string exp) 
+        { 
+            this.size = size;
+            this.name = name;
+            this.exp = exp;
+        }
+        public int GetSize()
+        {
+            return size;
+        }
+       
+    }
+
+}
+
+
+
+// SPS ZA LABU PAPAWA
