88f0687ace5291bf3b85909caba3d9fceadb8fe8
Artyom <47700409+ALWAYSWANNATILT@users.noreply.github.com>
2020-11-10 23:44:14 +0300

    Add files via upload

---
+++ lab 2/3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.CodeDom.Compiler;
+using System.Diagnostics.PerformanceData;
+
+namespace firstTSK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] symbolCount = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15,16,17 };
+            int[] wordCount = new int[symbolCount.Length];
+            string directory = Directory.GetCurrentDirectory() + "/";
+            string fileName = "file.txt";
+            using (StreamReader fs = new StreamReader($"{directory}{fileName}"))
+            {
+                while (!fs.EndOfStream)
+                {
+                    string[] temp = fs.ReadLine().Split(' ');
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        int count = temp[i].Count();
+                        Console.WriteLine($"{temp[i]} : {count}");
+                        for (int j = 0; j < symbolCount.Length; j++)
+                            if (count == symbolCount[j])
+                                wordCount[j]++;
+                    }
+                }
+            }
+            for (int i = 0; i < symbolCount.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} буквенных : {wordCount[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab 2/3_2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.CodeDom.Compiler;
+using System.Diagnostics.PerformanceData;
+using System.Runtime.CompilerServices;
+
+namespace secondTSK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory1 = Directory.GetCurrentDirectory() + @"\";
+            string directory2 = @"D:\";
+            string firstFile = "";
+            string secondFile = "";
+            Console.WriteLine("Введите команду:");
+            string[] command = Console.ReadLine().Split(' ');
+            bool fCheck = false, iCheck = false, nCheck = false;
+            if (command.Length < 3)
+                return;
+            for (int i = 0; i < command.Length; i++)
+            {
+                if (i == 0)
+                    if (command[i] != "cp")
+                        return;
+                if (command[i] == "-n")
+                    nCheck = true;
+                if (command[i] == "-f")
+                    fCheck = true;
+                if (command[i] == "-i")
+                    iCheck = true;
+                if (iCheck == true && nCheck == true)
+                    iCheck = false;
+                if (i == command.Length - 1)
+                    secondFile += command[i];
+                if (i == command.Length - 2)
+                    firstFile += command[i];
+            }
+            FileInfo fileInf = new FileInfo(directory1 + firstFile);
+            bool rewrite = true;
+            string temp = "";
+            if (fCheck)
+            {
+                if (nCheck)
+                {
+                    Console.WriteLine("Перезаписать файл(Y(англ)=да)");
+                    temp = Console.ReadLine();
+                    rewrite = false;
+                }
+                else
+                {
+                    if (iCheck)
+                    {
+                        Console.WriteLine("Перезаписать файл(Y(англ)=да)");
+                        temp = Console.ReadLine();
+                        if (temp == "Y")
+                            rewrite = true;
+                        else
+                            rewrite = false;
+                    }
+                    else
+                    {
+                        rewrite = true;
+                    }
+                }
+                try
+                {
+                    fileInf.CopyTo(directory2 + secondFile, rewrite);
+                }
+                catch (Exception ex)
+                {
+                    File.Delete(directory2 + secondFile);
+                    fileInf.CopyTo(directory2 + secondFile, true);
+                }
+            }
+            else
+            {
+                if (nCheck)
+                {
+                    Console.WriteLine("Перезаписать файл(Y(англ)=да)");
+                    temp = Console.ReadLine();
+                    rewrite = false;
+                }
+                else
+                {
+                    if (iCheck)
+                    {
+                        Console.WriteLine("Перезаписать файл(Y(англ)=да)");
+                        temp = Console.ReadLine();
+                        if (temp == "Y")
+                            rewrite = true;
+                        else
+                            rewrite = false;
+                    }
+                    else
+                    {
+                        rewrite = true;
+                    }
+                }
+                fileInf.CopyTo(directory2 + secondFile, rewrite);
+            }
+        }
+    }
+}
