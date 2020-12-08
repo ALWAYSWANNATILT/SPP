09488ac4f0b10a72971adf4a078d2dc53c6b51c9
Artyom <47700409+ALWAYSWANNATILT@users.noreply.github.com>
2020-12-08 22:47:56 +0300

    Add files via upload

---
+++ lab4/lab4_1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Department
+{
+    private String name;
+    private int count;
+    private class Position
+    {
+       public String title;
+       public int salary;
+        public override string ToString()
+           {
+                return "Position: " + title + "\nSalary: " + salary + "$\n";
+           }
+    }
+    private class Employee
+    {
+        public String name;
+        public String surname;
+        public int age;
+        public String employeePosition;
+        public int employeeSalary;
+        public override string ToString()
+        {
+            return "Name" + name + "\nSurname: " + surname + "\nAge: " + age + "\nPosition: " + employeePosition + "\nSalary: " + employeeSalary + "\n";
+        }
+    }   
+    List<Position> positions = new List<Position>();
+    public void AddPosition(String _title, int _salary)
+    {
+        Position pos = new Position();
+        pos.title = _title;
+        pos.salary = _salary;
+        positions.Add(pos);
+    }
+
+    public void show()
+    {
+        Console.WriteLine("ALL positions: ");
+        Console.WriteLine();
+        foreach(Position pos in positions)
+        {
+            Console.WriteLine($"{pos.ToString()}");
+        }
+        Console.WriteLine("ALL employees: ");
+        Console.WriteLine();
+
+        foreach (Employee employee in employees)
+        {
+            Console.WriteLine($"{employee.ToString()}");
+        }
+    }
+    List<Employee> employees = new List<Employee>();
+    public void AddEmployee (String _name, String _surname, int _age, string _employeePosition)
+    {
+        foreach(Position pos in positions)
+        {
+            if (pos.title == _employeePosition)
+            {
+                Employee employee = new Employee();
+                employee.name = _name;
+                employee.surname = _surname;
+                employee.age = _age;
+                employee.employeePosition = _employeePosition;
+                employee.employeeSalary = pos.salary;
+                employees.Add(employee);
+            }
+        }
+    }
+
+}
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department department = new Department();
+            department.AddPosition("student", 2000);
+            department.AddPosition("seller", 5000);
+            department.AddEmployee("Artyom", "Novikevich", 20, "student");
+            department.AddEmployee("Gena", "Bukin", 43, "seller");
+            department.show();
+        }
+    }
+}
---
+++ lab4/lab4_2/lab4_2/lab4_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Word
+{
+    private String word;
+    public Word(String word)
+    {
+        this.word = word;
+    }
+    public String getWord()
+    {
+        return word;
+    }
+}
+
+
+class Paragraph
+{
+    List<Word>words = new List<Word>();
+    public void addWord(Word word)
+    {
+        words.Add(word);
+    }
+    public void printALL()
+    {
+        for (int i = 0; i < words.Count; i++)
+            Console.WriteLine(words[i].getWord() + " ");
+    }
+}
+
+namespace lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Word word1 = new Word("HELLO");
+            Word word2 = new Word("C#");
+            Word word3 = new Word("from");
+            Word word4 = new Word("BSTU");
+
+
+
+            Paragraph paragraph1 = new Paragraph();
+            paragraph1.addWord(word1);
+            paragraph1.addWord(word2);
+            paragraph1.addWord(word3);
+            paragraph1.addWord(word4);
+            paragraph1.printALL();
+
+         
+        }
+    }
+}
